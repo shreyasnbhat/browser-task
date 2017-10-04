87e0aada9c709b815097178384ab1bb188adec40
Shreyas Bhat <shreyasnbhat@gmail.com>
2017-10-04 14:44:20 +0530

    Code refactor

--- Download.java
+++ Download.java
@@ -1,34 +1,7 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Download extends DownloaderEvent {
 
     public Download(int noOfTasks, String taskUrl) {
-
-        this.noOfTasks = noOfTasks;
-        this.taskUrl = taskUrl;
-        this.totalTaskTime = 0;
-
-        this.taskTimes = new ArrayList<>();
-
-        Random rand = new Random();
-
-        // The task should also spawn threads = noOfTasks
-        for (int i = 0; i < noOfTasks; i++) {
-            int taskSize = rand.nextInt(901) + 100;
-            taskTimes.add(taskSize);
-            this.totalTaskTime += taskSize;
-        }
-
-        System.out.println("Total Size of Tasks= " + this.totalTaskTime);
-
-        for (int i = 0; i < noOfTasks; i++) {
-            Task task = new Task(taskTimes.get(i), taskUrl);
-            task.addOnCompletionListener(this);
-            Thread taskThread = new Thread(task);
-            taskThread.start();
-        }
-
+        super(noOfTasks, taskUrl);
     }
 
     @Override
--- DownloaderEvent.java
+++ DownloaderEvent.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class DownloaderEvent implements ThreadCompletionListener {
 
@@ -7,6 +8,32 @@ public abstract class DownloaderEvent implements ThreadCompletionListener {
     protected int totalTaskTime;
     protected int totalTaskTimeCompleted = 0;
     protected ArrayList<Integer> taskTimes;
+
+    public DownloaderEvent(int noOfTasks, String taskUrl) {
+        this.noOfTasks = noOfTasks;
+        this.taskUrl = taskUrl;
+        this.totalTaskTime = 0;
+        this.taskTimes = new ArrayList<>();
+
+        Random rand = new Random();
+
+        // The task should also spawn threads = noOfTasks
+        for (int i = 0; i < noOfTasks; i++) {
+            int taskSize = rand.nextInt(901) + 100;
+            taskTimes.add(taskSize);
+            this.totalTaskTime += taskSize;
+        }
+
+        System.out.println("Total Size of Tasks for " + taskUrl + " = " + this.totalTaskTime);
+
+        for (int i = 0; i < noOfTasks; i++) {
+            Task task = new Task(taskTimes.get(i), taskUrl);
+            task.addOnCompletionListener(this);
+            Thread taskThread = new Thread(task);
+            taskThread.start();
+        }
+
+    }
 
     @Override
     public void notifyOfThreadCompletion(Runnable runner) {
